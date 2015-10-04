fd017d61665a0d57d5184e79b555ca34e09fde4b
Tom Smyth <tom@sassafras.coop>
2015-10-04 01:39:49 -0400

    Added support for RAILS_ENV

--- lib/dj_cron.rb
+++ lib/dj_cron.rb
@@ -5,8 +5,9 @@ require "active_record"
 module DjCron
   class Enqueuer
     def initialize
+      rails_env = ENV.fetch("RAILS_ENV", "development")
       raise "Database config not found at config/database.yml" unless File.exists?("config/database.yml")
-      ActiveRecord::Base.establish_connection(YAML.load_file("config/database.yml")["development"])
+      ActiveRecord::Base.establish_connection(YAML.load_file("config/database.yml")[rails_env])
       Delayed::Worker.backend = :active_record
     end
 
